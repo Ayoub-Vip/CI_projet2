e526c65c46b61bad7daee62da3afd0ddc85ab176
GregFreddy <93208192+GregFreddy@users.noreply.github.com>
2021-12-07 16:50:55 +0100

    Update shunting-yard.c

--- shunting-yard.c
+++ shunting-yard.c
@@ -56,22 +56,23 @@ int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 			 }
 		 }
 		 
-		 
 		 else if(tokentype == T_OPERATOR)
 		 {
-			 char* symbol_op1 = tokenGetValue(token);
-			 token* token_top = stackTop(Sop);
+			 char* symbol_op1 = malloc(sizeof(char));
+			 symbol_op1 = tokenGetValue(token);
+			 Token* token_top = malloc(sizeof(Token));
+			 token_top = stackTop(Sop);
 			 
 			 while( tokenGetType(token_top) == T_OPERATOR )
 			 {
 				 int prec_op1 = stGetOperatorPrec(st, symbol_op1);
 				 int prec_op2 = stGetOperatorPrec(st, tokenGetValue(token_top));
-				 int assoc_op2 = stGetOperatorAssoc(st, tokenGetValue(token_top)); 
+				 int assoc_op1 = stGetOperatorAssoc(st, symbol_op1); 
 				 
 				 if(prec_op1 == -1 || prec_op2 == -1 || assoc_op2 == -1)
 					 exit(failure);
 				 
-				 if(prec_op2 > prec_op1 || (prec_op1 == prec_op2 && assoc_op2 == 0))
+				 if(prec_op2 > prec_op1 || (prec_op1 == prec_op2 && assoc_op1 == 0))
 				 {
 					 token_top = stackPop(Sop);
 					 double* v1 = stackPop(Sval);
@@ -79,7 +80,7 @@ int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 					 
 					 OperatorFctType f = stGetOperatorFct(st, tokenGetValue(token_top));
 					 double *val = malloc(sizeof(double));
-					 val = &f(*v2,*v1);
+					 *val = f(*v2,*v1);
 					 
 					 stackPush(Sval, val);
 				 }
@@ -89,10 +90,14 @@ int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 			 
 			 stackPush(Sop, token);
 			 
+			 free(symbol_op1);
+			 free(token_top);
+			 
 		 }
 		 
 		 else if(tokentype == T_RIGHTPAR)
 		 {
+			 Token* token_top = malloc(sizeof(Token));
 			 token_top = stackTop(Sop);
 			 
 			 while(tokenGetType(token_top) != T_LEFTPAR)
@@ -102,26 +107,42 @@ int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 					 double* v2 = stackPop(Sval);
 					 OperatorFctType f = stGetOperatorFct(st, tokenGetValue(token));
 					 double* val = malloc(sizeof(double));
-					 val = &f(*v2, *v1)
+					 *val = f(*v2, *v1);
 					 stackPush(Sval,val);
 			 }
 			 
-			 token* token_L= stackPop(Sop);
+			 Token* token_L= malloc(sizeof(Token));
+			 token_L = stackPop(Sop);
 			 
 			 if(tokenGetType(stackTop(Sop)) == T_SYMBOL)
 			 {
 				 double *v = stackPop(Sval);
 				 FunctionFctType f = stGetFunctionFct(st, tokenGetValue(stackTop(Sop));
 				 double* val = malloc(sizeof(double));
-				 val = &f(*v)
+				 *val = f(*v)
 				 stackPush(Sval, val);
 			 }
-		 }			 
 			 
+			 free(token_top);
+			 free(token_L);
+		 }
+		 
+		 while(tokenGetType(stackTop(Sop)) == T_OPERATOR)
 			 
-			
+		 {
+			 Token* token_top = malloc(sizeof(Token));
+			 token_top = StackPop(Sop);
 			 
+			 double* v1 = stackPop(Sval);
+			 double* v2 = stackPop(Sval);
+			 
+			 FunctionFctType f = stGetFunctionFct(st, tokenGetValue(token_top));
+			 double* val  = malloc(sizeof(double));
+			 
+			 *val = f(*v2, *v1);
+			 
+			 stackPush(Sval, val);
+			 free(token_top);
+		 }
+	 
 		
-			 
-			 
-			 
