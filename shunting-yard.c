58a1d2f793180e55dfd6ac07a2d2b5afe5a52762
Ayoub ASSAOUD <86426829+Ayoub-Vip@users.noreply.github.com>
2021-12-11 02:32:20 +0100

    Update shunting-yard.c

--- shunting-yard.c
+++ shunting-yard.c
@@ -1,5 +1,6 @@
 #include "shunting-yard.h"
 #include "symboltable.h"
+#include "tokenizer.h"
 #include "list.h"
 #include "dict.h"
 #include "stack.h"
@@ -9,138 +10,166 @@
 // {
 // 	SymbolTable* st = stCreate();
 // 	return st;
-	
+
 // }
 int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 {
 	Stack *Sop  = stackCreate();
 	Stack *Sval = stackCreate();
+	List  *list = listCreate();
 
 	Token * token = tokenizerGetNextToken(tokenizer);
 	TokenType tokentype;
 
-	while( tokenGetType(token) != T_STOP )
+
+	while( tokenGetType(token) != T_STOP)
 	{
+
+		fprintf(stderr, "while principle\n");
+
 		 tokentype = tokenGetType(token);
+		// fprintf(stderr, "tokentype:%d \n",tokentype);
+		 
 		 if(tokentype == T_NUMBER)
 		 {
-			double *a = (double*) tokenGetValue(token);
+			double *a = malloc(sizeof(double));
+			listAdd(list, a);
+			*a =  *((double*) tokenGetValue(token));
 			stackPush(Sval, a);
+			// fprintf(stderr, "le variable %f!\n", *((double*)stackTop(Sval)));
 		 }
+
 		 else if(tokentype == T_LEFTPAR)
 		 {
 			stackPush(Sop, token);
 		 }
+
 		 else if(tokentype == T_SYMBOL)
 		 {
 		 	//tokenizerGetNextToken -> pos++
-		 	//Token *tkn_symbol_aux = token;
 		 	Token* token_next = tokenizerGetNextToken(tokenizer);
 
 			if(tokenGetType(token_next) == T_LEFTPAR)
 			{
 				stackPush(Sop, token);
-				//stackPush(Sop, token);
+				stackPush(Sop, token_next);
 			}
-			 else
-			 {
+			else
+			{
 				double *result = malloc(sizeof(double));
+				listAdd(list, result);
 				int a = stGetVariableValue(st, tokenGetValue(token),result);
-				 
+
 				if(a){
-							stackPush(Sval, result);
-							continue;
+					stackPush(Sval, result);
+					token = token_next;
+					continue;
 				}
-				
+
 				else{
-						    // fprintf(stderr, "le variable \' %s \'n'a pas ete declare!\n", tokenGetValue(token));
-						    exit(-1);
+				    fprintf(stderr, "le variable s n'a pas ete declare!\n");
+					exit(-1);
 					}
 
 			 }
 		 }
-		 
+
 		 else if(tokentype == T_OPERATOR)
 		 {
-			 char* symbol_op1 = malloc(sizeof(char));
-			 symbol_op1 = tokenGetValue(token);
+			 // char* symbol_op1 = malloc(sizeof(char));
+			 // listAdd(list,symbol_op1);
 			 // Token* token_top = malloc(sizeof(Token));
-			 
+
+			 char *symbol_op1 = tokenGetValue(token);
 			 //Token* token_top = stackTop(Sop);
-			 
+
 			 while( !(stackIsEmpty(Sop)) && tokenGetType(stackTop(Sop)) == T_OPERATOR )
 			 {
+		fprintf(stderr, "while 1\n");
+
 				 Token* token_top = stackTop(Sop);
 				 int prec_op1 = stGetOperatorPrec(st, symbol_op1);
 				 int prec_op2 = stGetOperatorPrec(st, tokenGetValue(token_top));
 				 int assoc_op1 = stGetOperatorAssoc(st, symbol_op1); 
-				 
-				 if(prec_op1 == -1 || prec_op2 == -1 || assoc_op1 == -1)
-					 exit(EXIT_FAILURE);
-				 
-				 if(prec_op2 > prec_op1 || (prec_op1 == prec_op2 && assoc_op1 == 0))
+
+				 if (prec_op1 == -1 || prec_op2 == -1 || assoc_op1 == -1) {
+				 			 exit(EXIT_FAILURE);
+				 			 fprintf(stderr, "les precedances \' s \'n'ont pas ete declare!\n");
+				 			}
+
+				 if (prec_op2 > prec_op1 || (prec_op1 == prec_op2 && assoc_op1 == 0))
 				 {
+				 	 if(stackIsEmpty(Sop)){
+				 	 	fprintf(stderr, "le Sop n'a pas ete trouve!\n");
+				 	 		 exit(EXIT_FAILURE);
+				 	 }
+
 					 token_top = stackPop(Sop);
-					 
-					 if(stackIsEmpty(Sval))
-						 exit(EXIT_FAILURE);
-					 
+
+					 if(stackIsEmpty(Sval)){
+					 	 exit(EXIT_FAILURE);
+					 }
 					 double* v1 = stackPop(Sval);
-					 
-					 if(stackIsEmpty(Sval));
-					 exit(EXIT_FAILURE);
-					 
+
+					 if(stackIsEmpty(Sval)){
+					  	exit(EXIT_FAILURE);}
+
 					 double* v2 = stackPop(Sval);
-					 
+
 					 OperatorFctType f = stGetOperatorFct(st, tokenGetValue(token_top));
+
 					 double *val = malloc(sizeof(double));
+					 listAdd(list, val);
+
 					 *val = f(*v2,*v1);
-					 
+
 					 stackPush(Sval, val);
 				 }
-				 
+
 				 //token_top = stackTop(Sop);
 			 }
-			 
+
 			 stackPush(Sop, token);
-			 
-			 free(symbol_op1);
-			 
+			 // stackPush(Sop, tokenGetValue(token));
+
+			 // free(symbol_op1);
+
 		 }
-		 
+
 		 else if(tokentype == T_RIGHTPAR)
 		 {
 			 // Token* token_top = malloc(sizeof(Token));
 			 //Token* token_top = stackTop(Sop);
-			 
-			 if(stackIsEmpty(Sop))
-				 exit(EXIT_FAILURE);
-			 
+
+			 if(stackIsEmpty(Sop)){
+			 				 exit(EXIT_FAILURE);}
+
 			 while(tokenGetType(stackTop(Sop)) != T_LEFTPAR)
 			 {
-				     
+		fprintf(stderr, "while 2\n");
+
 				 	 Token* token_top = stackPop(Sop);
-					 
-					 if(stackIsEmpty(Sop))
-						 exit(EXIT_FAILURE);
-					 
-					 if(stackIsEmpty(Sval))
-						 exit(EXIT_FAILURE);
+
+					 if(stackIsEmpty(Sop)){
+					 	 exit(EXIT_FAILURE);}
+
+					 if(stackIsEmpty(Sval)){
+					 	 exit(EXIT_FAILURE);}
 					 double* v1 = stackPop(Sval);
-					 
-					 if(stackIsEmpty(Sval))
-						 exit(EXIT_FAILURE);
+
+					 if(stackIsEmpty(Sval)){
+					 	 exit(EXIT_FAILURE);}
 					 double* v2 = stackPop(Sval);
-					 
+
 					 OperatorFctType f = stGetOperatorFct(st, tokenGetValue(token_top));
 					 double* val = malloc(sizeof(double));
 					 *val = f(*v2, *v1);
 					 stackPush(Sval,val);
 			 }
-			 
+
 			 // Token* token_L= malloc(sizeof(Token));
 			 Token* token_L = stackPop(Sop);
-			 
+
 			 if(!(stackIsEmpty(Sop)) && tokenGetType(stackTop(Sop)) == T_SYMBOL)
 			 {
 				 double *v = stackPop(Sval);
@@ -150,36 +179,40 @@ int syEvaluate(Tokenizer *tokenizer, SymbolTable *st, double* solution)
 				 stackPush(Sval, val);
 			 }
 		 }
-		 
+
 		 while( !(stackIsEmpty(Sop)) && tokenGetType(stackTop(Sop)) == T_OPERATOR)
-			 
 		 {
+		fprintf(stderr, "while 3\n");
+
 			 // Token* token_top = malloc(sizeof(Token));
 			 Token* token_top = stackPop(Sop);
-			 
+
 			 double* v1 = stackPop(Sval);
 			 double* v2 = stackPop(Sval);
-			 
+
 			 OperatorFctType f = stGetOperatorFct(st, tokenGetValue(token_top));
 			 double* val  = malloc(sizeof(double));
-			 
+
 			 *val = f(*v2, *v1);
-			 
+
 			 stackPush(Sval, val);
 		 }
+		 token = tokenizerGetNextToken(tokenizer);
 	}
-	
+	// listFree(list);
+
 	if(stackIsEmpty(Sop))
 		return 0;
 	else
 	{
-		solution = stackPop(Sval);
-		
 		if(stackIsEmpty(Sval))
 			return 0;
-		else
-			return 1
+		else{
+				*solution = (double) *((double*)stackPop(Sval));
+				return 1;
+			}
+
 	}
+
+
 }
-	 
-		
